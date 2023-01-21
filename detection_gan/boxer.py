b212b0209d2a308379087790c9f5c05bee2ea55e
Hubert Plisiecki <hplisiecki@gmail.com>
2023-01-21 14:01:59 +0100

    boxer

---
+++ detection_gan/boxer.py
@@ -0,0 +1,8 @@
+
+
+# function that will get a vector of numbers in the range of 0 to 1 and the response scale, and will box
+# the numbers into the response scale
+def box_numbers(numbers = [0.1, 0.6, 0.9], response_scale = [7, 5, 3]):
+
+
+    
