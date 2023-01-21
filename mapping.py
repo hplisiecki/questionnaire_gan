a68dade6ed72838623522763c3d2a048a3282387
Paweł Lenartowicz <6quarg@gmail.com>
2023-01-21 17:27:16 +0100

    1

---
+++ mapping.py
@@ -0,0 +1,15 @@
+def mapping(variable, n):
+    if n == 2:
+        if variable < 0: return(0)
+        else: return(0)
+    elif n == 3:
+        if variable < -1: return(0)
+        elif variable < 1: return(1)
+        else: return(2)
+    else:
+        for i in range(n):
+            print("bla")
+
+
+
+
