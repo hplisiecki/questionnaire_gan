73bc45e7d9a81a2020ee471648c61d4e3353d19a
Paweł Lenartowicz <6quarg@gmail.com>
2023-01-21 10:57:04 +0100

    1

--- generator of data.py
+++ generator of data.py
@@ -1,6 +1,7 @@
 import numpy as np
 rng = np.random.default_rng()
 import pandas as pd
+import torch
 
 # Random number of items
 n=rng.poisson(lam=8)+1
@@ -13,25 +14,17 @@ b = 2*np.random.random(size=(n,n))-1
 b_symm = (b + b.T)/2
 
 # Matrix of responses
-responses = rng.multivariate_normal(means, b_symm, check_valid="ignore", size=300)
+responses = rng.multivariate_normal(means, b_symm, check_valid="ignore", size=300, method="cholesky")
 responses = pd.DataFrame(responses)
 
 # Empirical values
 correlations = responses.corr()
 means = responses.mean()
 
-# Order
+# Add Order
 responses.insert(0, 'Order', range(1, 1+len(responses)))
 
-
 # Junk
-# Random
-junk_random = 2*np.random.random(size=(2,n))-1
-junk_random = pd.DataFrame(junk)
-
-#Glat
-
-
-
-
-print(junk)
+# Random responses
+b_random = torch.eye(n)
+responses = rng.multivariate_normal(means, b_symm, check_valid="ignore", size=300, method="cholesky")
